0236f6ad58e20c14c1c8dca425249274ef85ce53
VivekJJadav <150367108+VivekJJadav@users.noreply.github.com>
2025-01-01 18:22:55 +0530

    m

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { SignInCard } from "@/app/features/(auth)/components/sign-in-card";
+import { Suspense } from "react";
 
 const SignInPage = () => {
-  return <SignInCard />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          <div>Loading...</div>
+        </div>
+      }
+    >
+      <SignInCard />
+    </Suspense>
+  );
 };
 
 export default SignInPage;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { SignUpCard } from "@/app/features/(auth)/components/sign-up-card";
+import { Suspense } from "react";
 
 const SignUpPage = () => {
-  return <SignUpCard />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          <div>Loading...</div>
+        </div>
+      }
+    >
+      <SignUpCard />
+    </Suspense>
+  );
 };
 
 export default SignUpPage;
